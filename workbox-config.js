f61bf37e945f21d001135b6473e15563616381a7
Lukasz Walkow <lwalkow@atlassian.com>
2026-03-17 09:45:22 +1100

    Regenerate service worker and auto-update on commit
    - Regenerated sw.js with updated file hashes for cache busting
    - Removed stale workbox runtime files
    - Updated workbox-config to exclude test files and node_modules
    - Pre-commit hook now auto-regenerates service worker and cleans old workbox files

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{css,html,js,md,ttf,png,json,jpg}'
+		'**/*.{css,html,js,png,json,jpg}'
+	],
+	globIgnores: [
+		'node_modules/**',
+		'*.test.js',
+		'package.json',
+		'package-lock.json',
+		'workbox-config.js'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
