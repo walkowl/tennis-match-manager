2c002d911097fbc3d33f8dc9dbf00a17e8b14a69
Lukasz Walkow <lwalkow@atlassian.com>
2026-03-17 13:25:23 +1100

    Increase modal button sizes for iPad touch friendliness

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const APP_VERSION_DATE = '2026-03-17 12:59';
+const APP_VERSION_DATE = '2026-03-17 13:25';
 
 let createMatchesButton = document.getElementById('create-matches');
 let playerMatchCounts = {};
